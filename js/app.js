221153439252aa411cbf31db2b5f5cfbc3407723
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-01-27 14:14:42 -0600

    Primera versión funcional

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-require('dotenv').config()
-const Botgram = require('botgram');
+require('dotenv').config();
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
@@ -8,17 +7,58 @@ if (!TELEGRAM_BOT_TOKEN) {
     process.exit(1);
 }
 
-const bot = new Botgram(TELEGRAM_BOT_TOKEN);
-
-function onMessage(msg, reply) {
-    let text = msg.text.toLowerCase().replace(/[^\w\s\d]/gi, "");
-
-    console.log(msg);
-    if (text === 'puto') {
-        reply.text('Tú')
-    } else {
-        reply.text(msg.text);
+const fs = require('fs');
+fs.readFile('js/responses.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(`Error leyendo el archivo: ${err}`);
+        process.exit(1);
     }
-}
 
-bot.text(onMessage);
+    const responses = JSON.parse(data);
+    const Botgram = require('botgram');
+    const bot = new Botgram(TELEGRAM_BOT_TOKEN);
+
+    /**
+     * Da formato a la entrada del mensaje
+     * @param {string} input 
+     * @returns entrada normalizada
+     */
+    function formatInput(input) {
+        // Se eliminan los acentos
+        let text = input.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+        // El mensaje se pone en minúsculas
+        return text.toLowerCase().replace(/[^\w\s\d]/gi, '');
+    }
+
+    /**
+     * Compara la entrada del usuario con las posibles entradas
+     * @param {string} text 
+     * @returns respuesta en base a la entrada
+     */
+    function compare(text) {
+        for (let i = 0; i < responses.userInput.length; ++i) {
+            for (let j = 0; j < responses.botReplies.length; ++j) {
+                if (responses.userInput[i][j] === text) {
+                    let replies = responses.botReplies[i];
+                    let choice = Math.floor(Math.random() * replies.length);
+                    return replies[choice];
+                }
+            }
+        }
+        let choice = Math.floor(Math.random() * responses.alternative.length);
+        return responses.alternative[choice];
+    }
+
+    /**
+     * Acciones tomadas por el bot de Telegram
+     * @param {*} msg 
+     * @param {*} reply 
+     */
+    function onMessage(msg, reply) {
+        let input = formatInput(msg.text);
+        let response = compare(input);
+        reply.text(response);
+    }
+
+    bot.text(onMessage);
+});
